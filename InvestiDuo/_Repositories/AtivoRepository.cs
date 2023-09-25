78c6e39722f572becdcc2f8a23043cb42f8810ec
FeCanuto <felipee.canuto@hotmail.com>
2023-09-25 15:46:21 -0300

    Criação do repository

---
+++ InvestiDuo/_Repositories/AtivoRepository.cs
@@ -0,0 +1,88 @@
+﻿using InvestiDuo.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace InvestiDuo._Repositories
+{
+    public class AtivoRepository : BaseRepository, IAtivoRepository
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<AtivoModel> GetAll()
+        {
+            var assetList = new List<AtivoModel>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "Select * from Ativo order by Ativo_Id desc";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var assetModel = new AtivoModel();
+                        assetModel.Id = (int) reader[0];
+                        assetModel.Name = reader[1].ToString();
+                        assetModel.Ticket = reader[2].ToString();
+                        assetModel.Quantity = (int) reader[3];
+                        assetModel.Value = (decimal)reader[4];
+                        assetModel.Date = (DateTime)reader[5];
+                        assetModel.Total = (decimal)reader[6];
+
+                        assetList.Add(assetModel);
+                    }
+                }
+            }
+            return assetList;
+        }
+
+        public IEnumerable<AtivoModel> GetByValue(string value)
+        {
+            var assetList = new List<AtivoModel>();
+            int assetId = int.TryParse(value, out _) ? Convert.ToInt32(value) : 0;
+            string assetName = value;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "Select * from Ativo " +
+                    "where Ativo_Id = @id or Ativo_Name like @name+'%' order by Ativo_Id desc";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = assetId;
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = assetName;
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var assetModel = new AtivoModel();
+                        assetModel.Id = (int)reader[0];
+                        assetModel.Name = reader[1].ToString();
+                        assetModel.Ticket = reader[2].ToString();
+                        assetModel.Quantity = (int)reader[3];
+                        assetModel.Value = (decimal)reader[4];
+                        assetModel.Date = (DateTime)reader[5];
+                        assetModel.Total = (decimal)reader[6];
+
+                        assetList.Add(assetModel);
+                    }
+                }
+            }
+            return assetList;
+        }
+    }
+}
