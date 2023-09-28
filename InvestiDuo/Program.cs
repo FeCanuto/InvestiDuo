9a60a141b888e6ddca23bec489e52c8dfb04be8c
FeCanuto <felipee.canuto@hotmail.com>
2023-09-28 11:56:12 -0300

    Setando TrustServerCertificate para True

---
+++ InvestiDuo/Program.cs
@@ -0,0 +1,37 @@
+﻿using InvestiDuo._Repositories;
+using InvestiDuo.Models;
+using InvestiDuo.Presenters;
+using InvestiDuo.Views;
+using System.Configuration;
+
+namespace InvestiDuo
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            ApplicationConfiguration.Initialize();
+            Start();
+
+        }
+
+        public static void Start()
+        {
+            string sqlConnectionString = ConfigurationManager.ConnectionStrings["InvestiDuo.Properties.Settings.SqlConnection"].ConnectionString;
+            IAtivoView ativoView = new AtivoView();
+            IAtivoRepository ativoRepository = new AtivoRepository(sqlConnectionString);
+            new AtivoPresenter(ativoView, ativoRepository);
+            Application.Run((Form)ativoView);
+
+        }
+    }
+}
--- InvestiDuo/Properties/Settings.Designer.cs
+++ InvestiDuo/Properties/Settings.Designer.cs
@@ -26,7 +26,8 @@ namespace InvestiDuo.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost;Initial Catalog=InvestDuoDb;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost;Initial Catalog=InvestDuoDb;Integrated Security=true ;Trust" +
+            "ServerCertificate=true")]
         public string SqlConnection {
             get {
                 return ((string)(this["SqlConnection"]));
